4875607da0d270b9f48bed9311b802d3552b3255
Robert Lombardo <robert@wheelwell.com>
2019-06-04 01:54:54 +0900

    Initial commit - basic functionality for all data types (arrays, nested objs, etc)

---
+++ README.md
@@ -0,0 +1,40 @@
+## Watch Me Now
+_the teeny-tiny, zero-dependency object observer_
+* emits on property assignments for all data types
+* emits on mutable `Array` function calls
+* works on nested properties (infinitely)
+* works in node as well as the browser!
+
+#### installation:
+`npm install watch-me-now --save`
+
+#### example usage:
+```javascript
+const WatchMeNow = require(`watch-me-now`)
+
+// define a callback for when changes occur
+const onStateChange = ({type, current, old, path}) => {
+    console.log(`\nonStateChange() - ${type}, path: ${path}`)
+    console.log({current: JSON.stringify(current)})
+    console.log({old: JSON.stringify(old)})
+}
+
+// instantiate an object to observe
+const state = WatchMeNow({}, onStateChange)
+
+// go nuts
+state.foo = `bar`
+state.foo = {bar: `baz`}
+state.foo.bar = `bum`
+state.foo.bar = {bum: `filly`}
+state.foo.bar.bum = {filly: `narnia`}
+state.foo.bar.bum.filly = {narnia: 24601}
+
+state.my_array = [1, 2, 3, 4, 5]
+state.my_array[0] = `potato`
+state.my_array.reverse()
+
+state.foo = {nested_array: [`a`, `b`, `c`]}
+state.foo.nested_array.reverse()
+state.foo.nested_array.push({six_times_seven: 42})
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+const WatchMeNow = (me, changeCallback, path=``) => {
+    return new Proxy(me, {
+        set: (watched, key, val) => {
+            if (typeof val === `function`) throw `WatchMeNow does not accept functions on observed objects`
+
+            const old = shallowCopy(watched)
+
+            switch (typeof val) {
+                case `object`:
+                    if (Array.isArray(val)) extendMutableArrayFuncs(val, watched, changeCallback, path)
+
+                    // proxy any nested objects as well 
+                    watched[key] = WatchMeNow(val, changeCallback, `${path}.${key}`)
+
+                    // override any nested arrays
+                    if (watched.propertyIsEnumerable(key)) {
+                        for (let nested_key in val) {
+                            const nested_val = val[nested_key]
+                            if (Array.isArray(nested_val)) extendMutableArrayFuncs(nested_val, val, changeCallback, path)
+                        }
+                    }
+
+                    break
+
+                default:
+                    watched[key] = val
+            }
+
+            changeCallback({
+                type    : `PROPERTY_SET`,
+                current : shallowCopy(watched), 
+                old,
+                path
+            })
+
+            return true
+        }
+    })
+}
+module.exports = WatchMeNow
+
+const shallowCopy = obj => JSON.parse(JSON.stringify(obj))
+
+const extendMutableArrayFuncs = (array, watched, changeCallback, path) => {
+    // extend mutable Array methods to invoke callback when changes are made
+    [
+        `fill`,
+        `pop`,
+        `push`,
+        `reverse`,
+        `shift`,
+        `sort`,
+        `splice`,
+        `unshift`,
+    ].forEach(func_name => {
+        Object.defineProperty(array, func_name, {
+            value: (...args) => {
+                const old = shallowCopy(watched)
+
+                // call normal Array behavior
+                const res = Array.prototype[func_name].apply(array, args)
+                
+                changeCallback({
+                    type    : `ARRAY_FUNC_${func_name}`,
+                    current : shallowCopy(watched),
+                    old,
+                    path,
+                })
+                
+                return res
+            },
+            writable: true
+        })
+    })
+}
